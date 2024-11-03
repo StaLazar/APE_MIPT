b229551685858e4f98cb13da00f5b71c653a4f00
Stanislav Lazarev <fitmanst@gmail.com>
2024-11-03 16:00:57 +0300

    Добавлено задание №2 из ДЗ №3
    Дополнительно скорректировано именование переменных для задания №1.

--- primer_c/hw_03/task_2.c
+++ primer_c/hw_03/task_2.c
@@ -3,6 +3,7 @@
 int main(void) {
     int numbers[3] = {0, 0, 0};
     int sum = 0;
+    int product = 1;
 
     printf("Input 3 numbers:\n");
     scanf("%d %d %d", &numbers[0], &numbers[1], &numbers[2]);
@@ -10,10 +11,13 @@ int main(void) {
     const size_t count = sizeof(numbers) / sizeof(numbers[0]);
     for (size_t i = 0; i < count; ++i) {
         sum += numbers[i];
+        product *= numbers[i];
     }
 
     printf("\nThe sum of 3 numbers:\n");
     printf("%d + %d + %d = %d\n", numbers[0], numbers[1], numbers[2], sum);
+    printf("\nThe product of 3 numbers:\n");
+    printf("%d * %d * %d = %d\n", numbers[0], numbers[1], numbers[2], product);
 
     return 0;
 }

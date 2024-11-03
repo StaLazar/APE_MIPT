1d6a17e0257d414f7b0d8861a400ee672ad01656
Stanislav Lazarev <fitmanst@gmail.com>
2024-11-03 17:09:58 +0300

    Добавлено задание №4 из ДЗ №3

---
+++ primer_c/hw_03/task_4.c
@@ -0,0 +1,26 @@
+#include "stdio.h"
+
+int main(void) {
+    int number = 0;
+    int sum = 0;
+
+    printf("Input 3-digit number: ");
+    scanf("%d", &number);
+
+    const int lowerEdge = 100;
+    const int upperEdge = 999;
+    if ((number < lowerEdge) || (number > upperEdge)) {
+        printf("Interrupting: the number is out of range!\n");
+        return 1;
+    }
+
+    const int base = 10;
+    while (number != 0) {
+        sum += number % base;
+        number /= base;
+    }
+
+    printf("\nThe sum of number's digits: %d\n", sum);
+
+    return 0;
+}

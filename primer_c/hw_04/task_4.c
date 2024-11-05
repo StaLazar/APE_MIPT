448fb46a7b203f94fe1864cb9c04d2ead67179d6
Stanislav Lazarev <fitmanst@gmail.com>
2024-11-05 21:56:49 +0300

    Добавлено задание №4 из ДЗ №4

---
+++ primer_c/hw_04/task_4.c
@@ -0,0 +1,18 @@
+#include "stdio.h"
+
+int main(void) {
+    int numbers[5] = {0, 0, 0, 0, 0};
+    int max = 0;
+
+    printf("Input 5 numbers:\n");
+    scanf("%d %d %d %d %d", &numbers[0], &numbers[1], &numbers[2], &numbers[3], &numbers[4]);
+
+    const size_t count = sizeof(numbers) / sizeof(numbers[0]);
+    for (size_t i = 0; i < count; ++i) {
+        max = (max < numbers[i]) ? numbers[i] : max;
+    }
+
+    printf("\nThe max of 5 numbers: %d\n", max);
+
+    return 0;
+}

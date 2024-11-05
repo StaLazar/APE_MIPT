901dca23c00195d263c6f01982860fb8cf9926a5
Stanislav Lazarev <fitmanst@gmail.com>
2024-11-05 21:53:09 +0300

    Добавлено задание №3 из ДЗ №4

---
+++ primer_c/hw_04/task_3.c
@@ -0,0 +1,18 @@
+#include "stdio.h"
+
+int main(void) {
+    int numbers[3] = {0, 0, 0};
+    int max = 0;
+
+    printf("Input 3 numbers:\n");
+    scanf("%d %d %d", &numbers[0], &numbers[1], &numbers[2]);
+
+    const size_t count = sizeof(numbers) / sizeof(numbers[0]);
+    for (size_t i = 0; i < count; ++i) {
+        max = (max < numbers[i]) ? numbers[i] : max;
+    }
+
+    printf("\nThe max of 3 numbers: %d\n", max);
+
+    return 0;
+}

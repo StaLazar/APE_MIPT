5348173902a505ed23013b4b1f2f0bac3bebf99b
Stanislav Lazarev <fitmanst@gmail.com>
2024-11-06 22:33:36 +0300

    Добавлено задание №9 из ДЗ №4

---
+++ primer_c/hw_04/task_9.c
@@ -0,0 +1,23 @@
+#include "stdio.h"
+#include <stdbool.h>
+
+int main(void) {
+    int numbers[3] = {0, 0, 0};
+    bool isAscending = true;
+
+    printf("Input 3 numbers:\n");
+    scanf("%d %d %d", &numbers[0], &numbers[1], &numbers[2]);
+
+    const size_t count = sizeof(numbers) / sizeof(numbers[0]);
+    for (size_t i = 0, j = i + 1; j < count; ++i, ++j) {
+        if (numbers[i] >= numbers[j]) {
+            isAscending = false;
+            break;
+        }
+    }
+
+    printf("\nThe numbers are in ascending order: ");
+    isAscending ? printf("YES\n") : printf("NO\n");
+
+    return 0;
+}

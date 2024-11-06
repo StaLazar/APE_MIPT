d67993df5193eab8517d4e19b698d7530e73041c
Stanislav Lazarev <fitmanst@gmail.com>
2024-11-06 21:27:19 +0300

    Добавлено задание №7 из ДЗ №4

---
+++ primer_c/hw_04/task_7.c
@@ -0,0 +1,27 @@
+#include "stdio.h"
+
+int main(void) {
+    int number = 0;
+    int maxDigit = 0;
+
+    printf("Input 3-digit number: ");
+    scanf("%d", &number);
+
+    const int lowerEdge = 100;
+    const int upperEdge = 999;
+    if ((number < lowerEdge) || (upperEdge < number)) {
+        printf("Interrupting: the number is out of range!\n");
+        return 1;
+    }
+
+    const int base = 10;
+    while (number != 0) {
+        const int currentDigit = number % base;
+        maxDigit = (currentDigit > maxDigit) ? currentDigit : maxDigit;
+        number /= base;
+    }
+
+    printf("\nThe max of number's digits: %d\n", maxDigit);
+
+    return 0;
+}

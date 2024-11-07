88969a6378a6c8833987e52b62e7a78927828450
Stanislav Lazarev <fitmanst@gmail.com>
2024-11-07 21:26:26 +0300

    Добавлено задание №11 из ДЗ №4

---
+++ primer_c/hw_04/task_11.c
@@ -0,0 +1,19 @@
+#include "stdio.h"
+
+int main(void) {
+    int numbers[2] = {0, 0};
+
+    printf("Input 2 numbers:\n");
+    scanf("%d %d", &numbers[0], &numbers[1]);
+
+    printf("\nThe comparison result: ");
+    if (numbers[0] > numbers[1]) {
+        printf("above\n");
+    } else if (numbers[0] < numbers[1]) {
+        printf("less\n");
+    } else {
+        printf("equal\n");
+    }
+
+    return 0;
+}

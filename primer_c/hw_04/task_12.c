5073655ad210840c6ddf46766057b2ddbcee3355
Stanislav Lazarev <fitmanst@gmail.com>
2024-11-07 21:40:31 +0300

    Добавлено задание №12 из ДЗ №4

---
+++ primer_c/hw_04/task_12.c
@@ -0,0 +1,19 @@
+#include "stdio.h"
+
+int main(void) {
+    int sides[3] = {0, 0, 0};
+
+    printf("Input the triangle's potential sides:\n");
+    scanf("%d %d %d", &sides[0], &sides[1], &sides[2]);
+
+    printf("\nIs there a triangle: ");
+    if ((sides[0] + sides[1] > sides[2]) &&
+            (sides[1] + sides[2] > sides[0]) &&
+            (sides[2] + sides[0] > sides[1])) {
+        printf("yes\n");
+    } else {
+        printf("no\n");
+    }
+
+    return 0;
+}

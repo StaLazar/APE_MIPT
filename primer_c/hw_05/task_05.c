da260870fe848e019aa70c101ec2e46ce89cc73f
Stanislav Lazarev <fitmanst@gmail.com>
2024-11-09 13:59:53 +0300

    Добавлено задание №5 из ДЗ №5

---
+++ primer_c/hw_05/task_05.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void) {
+    int number = 0;
+    int sum = 0;
+
+    printf("Input a positive integer number: ");
+    scanf("%d", &number);
+
+    const int lowerEdge = 0;
+    if (number < lowerEdge) {
+        printf("Interrupting: the number is out of range!\n");
+        return 1;
+    }
+
+    /**
+     * @details do-while используется на случай, если ввели 0.
+     */
+    const int base = 10;
+    do {
+        sum += number % base;
+        number /= base;
+    } while (number != 0);
+
+    printf("\nThe sum of number's digits: %d\n", sum);
+
+    return 0;
+}

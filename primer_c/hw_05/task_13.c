5275f992e6b00e83cd7962434e9efc38c40b8cfa
Stanislav Lazarev <fitmanst@gmail.com>
2024-11-10 11:57:09 +0300

    Добавлено задание №13 из ДЗ №5

---
+++ primer_c/hw_05/task_13.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+#define BASE_10 10 //!< Основание десятичной системы счисления.
+
+int main(void) {
+    int number = 0;
+    int evenCount = 0;
+    int oddCount = 0;
+
+    printf("Input a non-negative integer number: ");
+    scanf("%d", &number);
+
+    const int lowerEdge = 0;
+    if (number < lowerEdge) {
+        printf("Interrupting: the number is out of range!\n");
+        return 1;
+    }
+
+    /**
+     * @details do-while используется на случай, если ввели 0.
+     */
+    do {
+        const int curDigit = number % BASE_10;
+        if (curDigit % 2 == 0) {
+            ++evenCount;
+        } else {
+            ++oddCount;
+        }
+        number /= BASE_10;
+    } while (number != 0);
+
+    printf("\nThe amount of even number's digit: %d\n", evenCount);
+    printf("The amount of odd number's digit: %d\n", oddCount);
+
+    return 0;
+}

d073f25dded5c2b9184149dac24e99f1e395cf94
Stanislav Lazarev <fitmanst@gmail.com>
2024-11-10 12:37:24 +0300

    Добавлено задание №15 из ДЗ №5
    Попутно скорректирован текст консольного вывода для задания №14 из ДЗ №5.

--- primer_c/hw_05/task_15.c
+++ primer_c/hw_05/task_15.c
@@ -2,15 +2,18 @@
 
 int main(void) {
     int number = 0;
-    int numCount = 0;
+    int evenCount = 0;
 
     const int excludingNum = 0;
     printf("Input a list of integer numbers ending with %d: ", excludingNum);
-    for (scanf("%d", &number); number != excludingNum; ++numCount) {
+    for (scanf("%d", &number); number != excludingNum;) {
+        if (number % 2 == 0) {
+            ++evenCount;
+        }
         scanf("%d", &number);
     }
 
-    printf("\nThe amount of numbers: %d\n", numCount);
+    printf("\nThe amount of even numbers: %d\n", evenCount);
 
     return 0;
 }

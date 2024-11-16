364175feff2caafc012f3c452f4797ebb4da4134
Stanislav Lazarev <fitmanst@gmail.com>
2024-11-16 19:13:35 +0300

    Добавлено задание №1 из ДЗ №6

---
+++ primer_c/hw_06/task_01.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * @brief Получить член геометрической прогресии.
+ * @param initial первый член прогрессии.
+ * @param ratio знаменатель прогрессии.
+ * @param number порядковый номер искомого члена прогрессии.
+ * @return Член геометрической прогрессии.
+ */
+double getGeomRateMember(int initial, int ratio, int number) {
+    return initial * pow(ratio, number - 1);
+}
+
+int main(void) {
+    int memberNum = 0;
+    const int lowerEdge = 1;
+    const int upperEdge = 64;
+
+    printf("Input a natural number in range [%d; %d]: ", lowerEdge, upperEdge);
+    scanf("%d", &memberNum);
+
+    if ((memberNum < lowerEdge) || (memberNum > upperEdge)) {
+        printf("Interrupting: the number is out of range!\n");
+        return 1;
+    }
+
+    const int initial = 1;
+    const int ratio = 2;
+    const double result = getGeomRateMember(initial, ratio, memberNum);
+    printf("\nGeometric progression's member: %.0f\n", result);
+
+    return 0;
+}

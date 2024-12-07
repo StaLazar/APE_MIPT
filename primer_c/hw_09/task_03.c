68b711620f66ab8e015dcc625fe9c3ec901af75e
Stanislav Lazarev <fitmanst@gmail.com>
2024-12-07 19:20:38 +0300

    Добавлено задание №3 из ДЗ №9

---
+++ primer_c/hw_09/task_03.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+//! Размер квадратной матрицы.
+#define SQ_MATRIX_SIZE 5
+
+/**
+ * @brief Заполнить квадратную матрицу значениями из консоли.
+ * @param matrix Квадратная матрица для заполнения.
+ */
+void fillSquareMatrix(int matrix[SQ_MATRIX_SIZE][SQ_MATRIX_SIZE]) {
+    printf("Input %d numbers:\n", SQ_MATRIX_SIZE * SQ_MATRIX_SIZE);
+    for (size_t i = 0; i < SQ_MATRIX_SIZE; ++i) {
+        for (size_t j = 0; j < SQ_MATRIX_SIZE; ++j) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+}
+
+/**
+ * @brief Получить след матрицы.
+ * @details След матрицы - это сумма элементов на ее главной диагонали.
+ * @param matrix Матрица, след которой необходимо получить.
+ * @return Искомый след матрицы.
+ */
+int getMatrixTrace(const int matrix[SQ_MATRIX_SIZE][SQ_MATRIX_SIZE]) {
+    int trace = 0;
+    for (size_t i = 0; i < SQ_MATRIX_SIZE; ++i) {
+        trace += matrix[i][i];
+    }
+    return trace;
+}
+
+int main(void) {
+    int matrix[SQ_MATRIX_SIZE][SQ_MATRIX_SIZE];
+    fillSquareMatrix(matrix);
+
+    const int trace = getMatrixTrace(matrix);
+
+    printf("\nThe matrix trace: %d\n", trace);
+
+    return 0;
+}

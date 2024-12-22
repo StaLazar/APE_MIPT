a2d2c388a0bb442cdafdd64e69c85392447b6349
Stanislav Lazarev <fitmanst@gmail.com>
2024-12-22 17:53:55 +0300

    КР: добавлены объявления типов и функций температуры

--- primer_c/coursework/sensors/temperature.c
+++ primer_c/coursework/sensors/temperature.c
@@ -1,1 +1,49 @@
 #include "temperature.h"
+
+temp_record getAverMonthTemp(const temp_data *records, const size_t size, const temp_time time) {
+    temp_record result = {0, false};
+    /**
+     * @todo Добавить реализацию.
+     */
+    return result;
+}
+
+temp_record getMinMonthTemp(const temp_data *records, const size_t size, const temp_time time) {
+    temp_record result = {0, false};
+    /**
+     * @todo Добавить реализацию.
+     */
+    return result;
+}
+
+temp_record getMaxMonthTemp(const temp_data *records, const size_t size, const temp_time time) {
+    temp_record result = {0, false};
+    /**
+     * @todo Добавить реализацию.
+     */
+    return result;
+}
+
+temp_record getAverAnnualTemp(const temp_data *records, const size_t size, const temp_time time) {
+    temp_record result = {0, false};
+    /**
+     * @todo Добавить реализацию.
+     */
+    return result;
+}
+
+temp_record getMinAnnualTemp(const temp_data *records, const size_t size, const temp_time time) {
+    temp_record result = {0, false};
+    /**
+     * @todo Добавить реализацию.
+     */
+    return result;
+}
+
+temp_record getMaxAnnualTemp(const temp_data *records, const size_t size, const temp_time time) {
+    temp_record result = {0, false};
+    /**
+     * @todo Добавить реализацию.
+     */
+    return result;
+}

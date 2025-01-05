f19a3e77a1527543b3ea72651aa8bd5fc19dfc78
Stanislav Lazarev <fitmanst@gmail.com>
2025-01-05 14:49:26 +0300

    Скорректирован тестовый код приложения

--- primer_c/coursework/apps/sensalyzer.c
+++ primer_c/coursework/apps/sensalyzer.c
@@ -1,10 +1,16 @@
-#include <stdio.h>
-
+#include "common/timestamp.h"
+#include "common/vector.h"
 #include "sensors/temperature.h"
 #include "tools/cmd_interface.h"
 
-int main () {
+#include <stdio.h>
+
+void test() {
     printf("Hello world!\n");
+}
+
+int main () {
+    test();
 
     return 0;
 }

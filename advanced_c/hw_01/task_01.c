9bae91c5fccc7d1d595c4662a165a1b90768ec0d
Stanislav Lazarev <fitmanst@gmail.com>
2025-01-29 20:29:37 +0300

    Добавлено задание №1 из ДЗ №1

---
+++ advanced_c/hw_01/task_01.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdint.h>
+
+//! Число бит в одном байте.
+#define BIT_PER_BYTE 8U
+
+int main(void) {
+    uint32_t num = 0U;
+    const uint32_t lowerNumEdge = 0U;
+
+    uint32_t shift = 0U;
+    const uint32_t lowerShiftEdge = 1U;
+    const uint32_t upperShiftEdge = 31U;
+
+    printf("Input an unsigned integer and a natural number not exceeding %u: ", upperShiftEdge);
+    scanf("%u %u", &num, &shift);
+
+    if ((num < lowerNumEdge) || (shift < lowerShiftEdge) || (shift > upperShiftEdge)) {
+        printf("Interrupting: the number is out of range!\n");
+        return 1;
+    }
+
+    const size_t numSize = BIT_PER_BYTE  * sizeof(num);
+    uint32_t result = (num >> shift) | (num << (numSize - shift));
+
+    printf("Result of cyclic shift: %u\n", result);
+    printf("%u\n", result);
+
+    return 0;
+}
